e79d840ffbdee940f46c5b77fe5c96b2b3f1b30c
Robert <roobtran@gmail.com>
2019-04-27 00:23:59 -0400

    +Added altar code + limb

---
+++ Ludum-Dare-44/Assets/Alter.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Alter : MonoBehaviour
+{
+    [SerializeField] GameObject panel;
+    [SerializeField] GameObject rightArm;
+    [SerializeField] GameObject leftArm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+  
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            panel.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            panel.SetActive(false);
+        }
+    }
+
+
+    public void RightArm()
+    {
+        rightArm.transform.localScale = new Vector3(0, 0, 0);
+    }
+
+    public void LeftArm()
+    {
+        leftArm.transform.localScale = new Vector3(0, 0, 0);
+    }
+
+}
