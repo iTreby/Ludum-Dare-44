b73b19f3e0b8a8315c30e1a958dba0ffc6a9dd56
Angeline <siv.angeline@ymail.com>
2019-04-29 18:31:25 -0400

    Added BridgeBehaviour script Fixed bridge Animation

---
+++ Ludum-Dare-44/Assets/Script/BridgeBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeBehaviour : MonoBehaviour
+{
+    [SerializeField] MainController countLimbs;
+    Animator bridgeAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bridgeAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(countLimbs.limbsGone == 4)
+        {
+            bridgeAnimator.SetBool("isActivated", true);
+        }
+    }
+}
