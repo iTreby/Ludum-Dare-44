b74e6d355c87de8b32688a4c8c3fb4688820c402
Robert <roobtran@gmail.com>
2019-04-29 19:59:29 -0400

    +Fixed Enemy

--- Ludum-Dare-44/Assets/Script/EnemyController.cs
+++ Ludum-Dare-44/Assets/Script/EnemyController.cs
@@ -25,7 +25,7 @@ public class EnemyController : MonoBehaviour
         float distance = Vector3.Distance(target.position, transform.position);
         if (distance <= lookRadius)
         {
-           // agent.SetDestination(target.position);
+            agent.SetDestination(target.position);
             if(distance <= agent.stoppingDistance)
             {
                 FaceTarget();
