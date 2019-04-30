c24d2cbdfd464d8d79add6ce6caf54c3a4b16794
Angeline <siv.angeline@ymail.com>
2019-04-29 20:11:53 -0400

    Added script that triggers ending

---
+++ Ludum-Dare-44/Assets/TriggerEnd.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TriggerEnd : MonoBehaviour
+{
+    GameObject endPanel;
+    GameObject UIPanel;
+
+    public void Awake()
+    {
+        endPanel.setActive(false);
+    }
+
+    public void OnCollisionEnter(Collision col)
+    {
+        if (col.gameobject.tag == "Player")
+        {
+            endPanel.setActive(true);
+            UIPanel.setActive(false);
+        }
+    }
+
+}
